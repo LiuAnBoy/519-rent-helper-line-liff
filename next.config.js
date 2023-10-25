38db8ed0fa00d2771cea03cda1ed10b678226b2f
LiuAnBoy <eric141886@gmail.com>
2023-10-25 23:49:43 +0800

    feat: user login

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const API_HOST = process.env.NEXT_PUBLIC_API_URL;
+const API_HOST = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 
 const nextConfig = {
   reactStrictMode: true,
